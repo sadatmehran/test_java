ddd1b515544642c7a9b1e8cbd57102955423683e
jamshid ahmadi <mehran.sadat2316@gmail.com>
2026-05-05 18:44:10 +0200

    create dog and monkey

---
+++ src/animals/dog.java
@@ -0,0 +1,10 @@
+public class Dog{
+    private string type;
+    private int age;
+    private string type;
+    public Dog(){
+        this.type = "animal";
+        this.age = 5;
+        this.color = "white";
+    }
+}
